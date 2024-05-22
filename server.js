4d62095975d0e74ee12d2ea9d82b83e71f6d2553
Praveen Kumar <pk46066@gmail.com>
2024-05-22 19:05:14 +0530

    user mvc apu

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import path from 'path'
-import bodyPaser from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+import bodyPaser from "express";
+import contactRouter from './routes/ContactUs.js'
+import userRouter from './routes/Users.js'
 
-import {insertContactUs,fetchAllContact,fetchContactbyid,updateContactbyid,deleteContactbyid} from './Controllers/contactus.js'
 
-const port =2000;
+const port = 2000;
 const app = express();
 
-app.use(bodyPaser.json())
-
-app.use(express.static(path.join(path.resolve(),'public')))
-
-app.listen(port,()=>console.log(`server is running from port ${port}`))
-
+// add these lines to accept req body for POST call
+app.use(bodyPaser.json());
+app.use(express.json())
 // ejs and express middlewire
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(path.resolve(), "public")));
+
+app.listen(port, () => console.log(`server is running from port ${port}`));
+
+
 
 // Mongodb connection
 
-mongoose.connect(
+mongoose
+  .connect(
     "mongodb+srv://pk46066:B4RMaGhqBBNHhxel@cluster0.ipmeqni.mongodb.net/",
     {
-       "dbName":"nodejs_express_api"
+      dbName: "nodejs_express_api",
     }
- ).then(
-    ()=>console.log("mongodb connected")
-    ).catch(
-       (error)=>console.log(error)
-    );
+  )
+  .then(() => console.log("mongodb connected"))
+  .catch((error) => console.log(error));
 
+app.use("/api/contact", contactRouter);
 
-
-app.post('/api/contact', insertContactUs)
-
-app.get('/api/fetch-contact-list/',fetchAllContact)
-
-app.get('/api/fetch-contact-list-byid/:id',fetchContactbyid)
-
-app.put('/api/update-contact-byid/:id',updateContactbyid)
- 
-app.delete('/api/delete-contact-byid/:id',deleteContactbyid)
- 
+app.use("/api/user", userRouter);
